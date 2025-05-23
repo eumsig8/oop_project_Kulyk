8194c9465a08f2562dce351b401deb548db4d86f
Mariia <mariia.kulyk@sa.stud.vu.lt>
2025-05-23 17:00:19 +0300

    updated register

--- register.php
+++ register.php
@@ -1,19 +1,29 @@
 <?php
-require_once "User.php";
 require_once "config.php";
+require_once "classes/Database.php";
+require_once "classes/User.php";
+
+session_start();
+
+$message = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = new User();
-    if ($user->register($_POST['username'], $_POST['password'])) {
-        echo "User registered successfully!";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($user->register($username, $password)) {
+        $message = "Registration successful. You can now <a href='login.php'>login</a>.";
     } else {
-        echo "Registration failed.";
+        $message = "Username already exists or registration failed.";
     }
 }
 ?>
 
+<h2>Register</h2>
+<p><?= $message ?></p>
 <form method="post">
-    <input type="text" name="username" placeholder="Username" required />
-    <input type="password" name="password" placeholder="Password" required />
+    <input type="text" name="username" placeholder="Username" required /><br><br>
+    <input type="password" name="password" placeholder="Password" required /><br><br>
     <button type="submit">Register</button>
 </form>
